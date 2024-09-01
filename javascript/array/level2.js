3f88be7cf830bc079c25ecbaa9cb3f4e5a70952d
SHREESTIP <shristipradhan2056@gmail.com>
2024-09-01 09:35:23 +0545

    level 2 answer

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -9,11 +9,12 @@ console.log(array.splice (1,2));
 //Q2
 //expected output is [5,3,5,3]
 
-console.log()
+console.log(array.reverse());
 
 
 //Q3
 const newArr = [2,1]
 //expected output is [5,3,5,3,2,1]
 
-console result = array.concat(newArr);
+const result = array.concat(newArr);
+console.log (result);
