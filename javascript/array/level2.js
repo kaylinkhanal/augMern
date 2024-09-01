6e4fca44d98dab341705016726e5d16e394f7d2f
adityaspaudel <adityaspaudel@gmail.com>
2024-09-01 09:02:37 +0545

    array reversed

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -1,5 +1,11 @@
 const array = [3, 5, 3, 5];
-//expected output is [5,3]
-array.pop();
-const newArray = array.slice(1);
-console.log(newArray);
+//Q1 expected output is [5,3]
+//array.pop();
+//const newArray = array.slice(1);
+//console.log(newArray);
+
+//-------------------------
+
+//Q2 expected [5,3,5,3]
+
+console.log(array.reverse());
