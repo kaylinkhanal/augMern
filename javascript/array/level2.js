2bdc81510ffdf64e222606cf0249a796457a77ef
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-01 09:06:24 +0545

    updated 2nd answer @level2 array

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -1,10 +1,10 @@
-// const array = [3, 5, 3, 5];
-// //expected output is [5,3]
-// console.log(array.slice(1, 3));
-// console.log(array.splice(1, 2));
-const array1 = [3, 5, 3, 5];
+const array = [3, 5, 3, 5];
 //Q1
 //expected output is [5,3]
+console.log(array.slice(1, 3));
+console.log(array.splice(1, 2));
+
+const array1 = [3, 5, 3, 5];
 
 //Q2
 //expected output is [5,3,5,3]
