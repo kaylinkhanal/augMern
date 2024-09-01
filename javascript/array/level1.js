1b621d1bcf6b1db04db602828dcbcd4c80449a34
adityaspaudel <adityaspaudel@gmail.com>
2024-09-01 09:02:37 +0545

    array reversed commit message changed

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -1,5 +1,5 @@
 const array = [2, 4, 5];
 // calculate the length of an array
 //expected output is 3
-const output = array.length;
-console.log(output);
+//const output = array.length;
+//console.log(output);
