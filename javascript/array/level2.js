f2dbdc260b57d2fa3165c62dc3e8a89b562b394d
Rockey C.R <rockeyrai234@gmail.com>
2024-09-01 09:05:48 +0545

    Q1 was commited

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -1,7 +1,7 @@
 const array = [3,5,3,5]
 //expected output is [5,3]
-let ans = array.splice(1,2)
-console.log(ans)
+// let ans = array.splice(1,2)
+// console.log(ans)
 //Q1
 //expected output is [5,3]
 
