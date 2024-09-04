0478e4178c39a4550437e17a6ffb73260b19d1d3
Rockey C.R <rockeyrai234@gmail.com>
2024-09-04 08:42:57 +0545

    1 and 2 answer of level 3

--- javascript/array/level3.js
+++ javascript/array/level3.js
@@ -2,10 +2,23 @@ const array = [3,5,2]
 
 //Q1) expected output 
 //10
-
+let sum = 0;
+array.forEach((nums) => {
+  sum += nums;
+});
 
 // Q2) expected output 
 // [4,6,2]
+let newNums = [];
+array.forEach((item) => {
+  if (item % 2 === 0) {
+    newNums = [...newNums, item];  
+  } else {
+    newNums = [...newNums, item + 1];  
+  }
+});
+
+console.log(newNums);
 
 
 //Q3) expected output:
