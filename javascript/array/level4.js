7f3e84d430a3da3c3026bebf7304cbb05d0c3a98
Dilasha Shakya <dilashakya@gmail.com>
2024-09-03 22:32:00 -0500

    level 4 changed a bit

--- javascript/array/level4.js
+++ javascript/array/level4.js
@@ -4,14 +4,14 @@ const user = {
     science:40,
     gk:40,
 }
-
 delete user.name
 console.log(user)
 const arr = Object.values(user)
 let sum = 0
-arr.forEach((items)=> sum + items)
+arr.forEach((items, id)=>{
+  sum = sum + items
+})
 console.log(sum)
-//Q1
 //hint: get only [30,40, 40 ] first
 // add all items to sum
 //expected output : 110
