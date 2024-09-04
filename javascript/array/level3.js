0bfbf6fac2c8269da5648fc5ab04f40ec26581a8
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-09-04 08:48:46 +0545

    Level 3 Q2

--- javascript/array/level3.js
+++ javascript/array/level3.js
@@ -1,20 +1,22 @@
-const array = [3,5,2]
+const array = [3, 5, 2];
 
-//Q1) expected output 
+//Q1) expected output
 //10
-let sum = 0
-array.forEach((item,id) => {
-    sum += item
+let sum = 0;
+array.forEach((item, id) => {
+  sum += item;
 });
-console.log(sum)
+console.log(sum);
 
-
-
-// Q2) expected output 
+// Q2) expected output
 // [4,6,2]
-
+const output = array.map((item, id) => {
+  if (item % 2 ===1) item = item +1
+  return item
+});
+console.log(output);
 
 //Q3) expected output:
 // [13,15]
 
-console.log(array)
+console.log(array);
