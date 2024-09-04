ff6f74728f731cda2f27a777f650dc74768cb9bf
adityaspaudel <adityaspaudel@gmail.com>
2024-09-04 08:45:11 +0545

    q1 & q2 solved

---
+++ javascript/array/level3.js
@@ -0,0 +1,24 @@
+const array = [3, 5, 2];
+
+//Q1) expected output
+//10
+let sum = 0;
+array.forEach((item, id) => {
+	sum += item;
+	return sum;
+});
+console.log(sum);
+// Q2) expected output
+// [4,6,2]
+
+const x = array.map((item) => {
+	if (item % 2 === 0) {
+		return item;
+	} else {
+		return (item += 1);
+	}
+});
+console.log(x);
+
+//Q3) expected output:
+// [13,15]
