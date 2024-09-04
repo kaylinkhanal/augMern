1580ab9b2d48f7a547669db37326dbc526497e9d
Kaylin Khanal <kyalinnop@gmail.com>
2024-09-04 09:00:45 +0545

    added level3

--- javascript/array/level3.js
+++ javascript/array/level3.js
@@ -1,10 +1,21 @@
 const array = [3,5,2]
 
 //Q1) expected output 
+let sum = 0
+array.forEach((item,id)=>{
+sum = sum + item
+})
+console.log(sum)
 //10
 
 
 // Q2) expected output 
+// increase by one if odd
+const output = array.map((item,id)=>{
+   if(item%2 ===1 ) item = item + 1
+   return item
+})
+console.log(output)
 // [4,6,2]
 
 
---
+++ javascript/array/level4.js
@@ -0,0 +1,13 @@
+const user = {
+    name :'ram',
+    math:30,
+    science:40,
+    gk:40,
+}
+
+
+//Q1
+//hint: get only [30,40, 40 ] first
+// add all items to sum
+//expected output : 110
+
