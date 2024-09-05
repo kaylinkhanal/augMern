9bba6517beb724b3512f54238d86c33cd5e17c11
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-05 08:46:01 +0545

    level 5 array question answered

--- javascript/array/level5.js
+++ javascript/array/level5.js
@@ -1,14 +1,19 @@
-const arr = ['ram','shyam', 'gopal']
+const arr = ["ram", "shyam", "gopal"];
 
 const user = {
-    'ram': 'thapa',
-    'gopal': 'bhatta'
-}
+  ram: "thapa",
+  gopal: "bhatta",
+};
 
 // expectedOUTPUT:
 // ['ram thapa', 'shyam', 'gopal bhatta']
-const output = arr.map((item)=>{
-    // hint: user['ram']
-})
+const output = arr.map((item) => {
+  // hint: user['ram']
+  if (typeof user[item] !== "undefined") {
+    return item + " " + user[item];
+  } else {
+    return item;
+  }
+});
 
-console.log(output)
+console.log(output);
--- javascript/array/level7.js
+++ javascript/array/level7.js
@@ -1,31 +1,43 @@
-const arr = [2,5,6,7]
+const arr = [2, 5, 6, 7];
 //calculate the sum of the array using either for of or for in
 
-
+let sum = 0;
+for (let x of arr) {
+  sum = sum + x;
+}
+console.log(sum);
 
 //high level
-const users =['hari', 'shyam', 'hari']
+const users = ["hari", "shyam", "hari"];
 //hint: how to know hari was already there in users array
 //hint2: how to remove particular index/item
 //remove from array if duplicate
 
-const newArr = [[4,5], [5,7], [7,2]]
+
+
+const newArr = [
+  [4, 5],
+  [5, 7],
+  [7, 2],
+];
 //calculate the sum of all the odd numbers inside this nested array
 
+let oddSum = 0;
+
+
 const myDetails = [
-    {id:3, name: 'hari'},
-    {id:5, name: 'shyam'},
-    {id:6, name: 'gopal'},
-  ]
-  
+  { id: 3, name: "hari" },
+  { id: 5, name: "shyam" },
+  { id: 6, name: "gopal" },
+];
+
 // return only array of ids: expected output  [3,5,6]
 
-
-const userDetails= [
-    {score: 0, name:'hari', marks: [10,3,23]},
-    {score: 0, name:'shyam', marks: [50,23,23]},
-    {score: 0, name:'shyam',marks: [20,13,43]},
-]
+const userDetails = [
+  { score: 0, name: "hari", marks: [10, 3, 23] },
+  { score: 0, name: "shyam", marks: [50, 23, 23] },
+  { score: 0, name: "shyam", marks: [20, 13, 43] },
+];
 //loop over the arr of objects and calculate total score, expected output is:
 
 // [
