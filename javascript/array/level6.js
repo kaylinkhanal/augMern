89feddd4610eaa3beee5393fd26943d265edb42a
Rockey C.R <rockeyrai234@gmail.com>
2024-09-05 14:47:32 +0545

    second try of array level 6

--- javascript/array/level6.js
+++ javascript/array/level6.js
@@ -29,3 +29,34 @@ const scores = {
 }
 
 //no of subjects the student has failed
+let scoresArr = Object.keys(scores)
+let failStudent = 0;
+let passStudent = 0;
+
+scoresArr.forEach((item)=>{
+  let newType = scores[item].type;
+  (config[newType].passMarks < scores[item].score)?  passStudent += 1: failStudent += 1
+})
+console.log(failStudent);
+//avarage pecentage 
+
+//sum of total makrs
+let totalMarks = 0;
+scoresArr.forEach((sub)=>{
+  if(scores[sub].type === "theory"){
+    totalMarks += config.theory.fullMarks
+  }else{
+    totalMarks += config.practical.fullMarks
+  }
+})
+
+//sum  of all subject score
+let userMarks = 0;
+
+scoresArr.forEach((marks)=>{
+  userMarks+=scores[marks].score
+})
+ // last average percentage of user
+
+let avgPercent = (userMarks / totalMarks)*100;
+parseFloat(avgPercent.toFixed(2))
