03cd85c90e8a32d831ba938c397bd796a971931d
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-09-05 08:57:16 +0545

    All captial

--- javascript/array/level5.js
+++ javascript/array/level5.js
@@ -9,10 +9,9 @@ const user = {
 // ['ram thapa', 'shyam', 'gopal bhatta']
 const output = arr.map(item => {
     if (user[item]) {
-        return item + ' ' + user[item];
+        return (item + ' ' + user[item]).toUpperCase();
     } else {
-        return item;
+        return item.toUpperCase();
     }
 });
-
 console.log(output)
