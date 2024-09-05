f919876625a5d010a65ad14e32474ae023c032f7
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-09-05 08:27:07 +0545

    Level 4 Q1

--- javascript/array/level4.js
+++ javascript/array/level4.js
@@ -1,14 +1,14 @@
 const user = {
-    name :'ram',
-    math:30,
-    science:40,
-    gk:40,
-}
-
-
+  name: "ram",
+  math: 30,
+  science: 40,
+  gk: 40,
+};
 
 //Q1
 //hint: get only [30,40, 40 ] first
 // add all items to sum
 //expected output : 110
-
+const scores = [user.math, user.science, user.gk];
+const sum = scores.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
