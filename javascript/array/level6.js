1793c7e41cee7112f73fc88ac902ba6db7d0cc8a
Dilasha Shakya <dilashakya@gmail.com>
2024-09-05 18:57:30 -0500

    level 6 q1 added

--- javascript/array/level6.js
+++ javascript/array/level6.js
@@ -27,5 +27,14 @@ const scores = {
     score: 20
    },
 }
-
+let number = 0
+Object.entries(scores).forEach(([subject, other])=>{
+  if (other.type === 'theory' && other.score < config.theory.passMarks ){
+    number +=1
+  }
+  if (other.type === 'practical' && other.score < config.practical.passMarks ){
+    number = number + 1
+  }
+});
+console.log(number)
 //no of subjects the student has failed
