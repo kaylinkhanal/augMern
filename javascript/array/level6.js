d4ad60d1e461c9e7d190b3c9cbb5f410548ca398
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-06 08:40:54 +0545

    Q1 &Q2 of array level 6 answered

--- javascript/array/level6.js
+++ javascript/array/level6.js
@@ -1,31 +1,57 @@
 const config = {
-    practical: {
-        passMarks: 30,
-        fullMarks: 50
-    },
-    theory: {
-        passMarks: 40,
-        fullMarks: 100
-    },
-}
+  practical: {
+    passMarks: 30,
+    fullMarks: 50,
+  },
+  theory: {
+    passMarks: 40,
+    fullMarks: 100,
+  },
+};
 
 const scores = {
-   math: {
-    type: 'theory',
-    score: 60
-   },
-   science: {
-    type: 'theory',
-    score: 20
-   },
-   computer: {
-    type: 'theory',
-    score: 30
-   },
-   lab:{
-    type: 'practical',
-    score: 20
-   },
-}
+  math: {
+    type: "theory",
+    score: 60,
+  },
+  science: {
+    type: "theory",
+    score: 20,
+  },
+  computer: {
+    type: "theory",
+    score: 30,
+  },
+  lab: {
+    type: "practical",
+    score: 20,
+  },
+};
 
 //no of subjects the student has failed
+// let count = 0;
+// for (const [key, value] of Object.entries(scores)) {
+//   if (value.score < config[value.type].passMarks) {
+//     count++;
+//   }
+// }
+// console.log(count);
+let count = 0;
+Object.entries(scores).forEach(([key, value]) => {
+  if (value.score < config[value.type].passMarks) {
+    count++;
+  }
+});
+console.log(count);
+
+//calculate the percentage of the student
+// let sum = 0;
+// for (const [key, value] of Object.entries(scores)) {
+//   sum += value.score;
+// }
+// console.log((sum / 350) * 100);
+let sum = 0;
+Object.entries(scores).forEach(([key, value]) => {
+  sum += value.score;
+});
+console.log((sum / 350) * 100);
