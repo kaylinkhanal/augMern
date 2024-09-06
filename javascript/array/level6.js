efa0ed39253e3d41b06d8a9e9e394bb499797e3c
Nabin Maharjan <maharjan.nabin85@gmail.com>
2024-09-06 08:43:09 +0545

    array level6 problem need to fix

--- javascript/array/level6.js
+++ javascript/array/level6.js
@@ -31,37 +31,38 @@ const scores = {
 //no of subjects the student has failed
 //calculate percentage scored
 
-const passMarksPractical = config.practical.passMarks;
-const passMarksTheory = config.theory.passMarks;
-const scoreEachSubject = Object.entries(scores);
-const fullMarksPractical = config.practical.fullMarks;
-const fullMarksTheory = config.theory.fullMarks;
-let subjectFailed = 0;
-let totalFullMarks = 0;
+// const passMarksPractical = config.practical.passMarks;
+// const passMarksTheory = config.theory.passMarks;
+// const scoreEachSubject = Object.entries(scores);
+// const fullMarksPractical = config.practical.fullMarks;
+// const fullMarksTheory = config.theory.fullMarks;
+// let subjectFailed = 0;
+// let totalFullMarks = 0;
 
-const totalScore = scoreEachSubject.map((item) => {
-  return item[1].score
-});
-totalScore.forEach((score) => {
-  if (score <= passMarksPractical && score <= passMarksTheory) {
-    subjectFailed ++
-  }
-})
-console.log(subjectFailed);
+// const totalScore = scoreEachSubject.map((item) => {
+//   return item[1].score
+// });
+// totalScore.forEach((score) => {
+//   if (score <= passMarksPractical && score <= passMarksTheory) {
+//     subjectFailed ++
+//   }
+// })
+// console.log(subjectFailed);
 
-const totalMarks = scoreEachSubject.map((item) => {
-  if (item[1].type === "theory") {
-    return fullMarksTheory
-  } else {
-    return fullMarksPractical
-  }
-}).forEach((marks) => {
-  totalFullMarks = totalFullMarks + marks
-})
+// const totalMarks = scoreEachSubject.map((item) => {
+//   if (item[1].type === "theory") {
+//     return fullMarksTheory
+//   } else {
+//     return fullMarksPractical
+//   }
+// }).forEach((marks) => {
+//   totalFullMarks = totalFullMarks + marks
+// })
 
-const percentageScored = totalScore.map((item) => {
-  return Math.round((item/totalFullMarks * 100) * 100)/100
-})
-console.log(percentageScored)
+// const percentageScored = totalScore.map((item) => {
+//   return Math.round((item/totalFullMarks * 100) * 100)/100
+// })
+// console.log(percentageScored)
+
 
 
