fb986a95eadd0c4488466136dcd8df76632cf9a1
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-09-06 08:35:35 +0545

    Level 6 Q1

--- javascript/array/level6.js
+++ javascript/array/level6.js
@@ -36,23 +36,32 @@ function calculateResults(scores) {
   let totalScore = 0;
   let totalFullMarks = 0;
 
-  for (const subject in scores) {
-    const { type, score } = scores[subject];
-    const { passMarks, fullMarks } = config[type];
+  for (let subject in scores) {
+    let subjectInfo = scores[subject];
+    let type = subjectInfo.type;
+    let score = subjectInfo.score;
+
+    let passMarks = config[type].passMarks;
+    let fullMarks = config[type].fullMarks;
 
     if (score < passMarks) {
       failedSubjects++;
     }
 
     totalScore += score;
+
     totalFullMarks += fullMarks;
   }
 
-  const percentage = (totalScore / totalFullMarks) * 100;
+  let percentage = (totalScore / totalFullMarks) * 100;
 
-  return { failedSubjects, percentage };
+  return {
+    failedSubjects: failedSubjects,
+    percentage: percentage,
+  };
 }
 
-const result = calculateResults(scores);
-console.log(`Failed Subjects: ${result.failedSubjects}`);
-console.log(`Percentage Scored: ${result.percentage.toFixed(2)}%`);
+let result = calculateResults(scores);
+
+console.log("Number of subjects failed: " + result.failedSubjects);
+console.log("Total percentage scored: " + result.percentage.toFixed(2) + "%");
