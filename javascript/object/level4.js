48e22edaf1aeafaa943be88f777e4bc27d1ab54f
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-08 07:36:51 +0545

    level4 @ object done

--- javascript/object/level4.js
+++ javascript/object/level4.js
@@ -1,16 +1,22 @@
 const userDetails = {
-   ram: {
-    nationality: 'nepal'
-},
-stephan: {
-    nationality: 'USA'
-}
-}
+  ram: {
+    nationality: "nepal",
+  },
+  stephan: {
+    nationality: "USA",
+  },
+};
 
-const country ={
-    nepal: 'kathmandu',
-    USA: 'washington DC'
-}
-
+const country = {
+  nepal: "kathmandu",
+  USA: "washington DC",
+};
 
 // Output should be [['ram', 'kathmandu'], ['stephan','washington DC']]
+const answer = Object.entries(userDetails).map((item) => {
+  const user = item[0];
+  const userNationality = item[1].nationality;
+  const output = [user, country[userNationality]];
+  return output;
+});
+console.log(answer);
