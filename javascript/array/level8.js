f0858b4712934328cc4ab334abff40502e85146d
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-08 22:38:22 +0545

    array level8 done

--- javascript/array/level8.js
+++ javascript/array/level8.js
@@ -1,7 +1,16 @@
-const cartItems= [
-    {id: 45, product:'hawkins', quantity:3, price:10},
-    {id: 42, product:'baltra cooker', quantity:5, price:20},
-    {id: 43, product:'phillips', quantity:2, price:10},
-    {id: 41, product:'nike shoes', quantity:1, price:20},
-]
+const cartItems = [
+  { id: 45, product: "hawkins", quantity: 3, price: 10 },
+  { id: 42, product: "baltra cooker", quantity: 5, price: 20 },
+  { id: 43, product: "phillips", quantity: 2, price: 10 },
+  { id: 41, product: "nike shoes", quantity: 1, price: 20 },
+];
 //total price using reduce
+let totalPrice = cartItems.reduce((acc, item) => {
+  return (acc += item.quantity * item.price);
+}, 0);
+console.log(totalPrice);
+
+// let withSubTotal = cartItems.map((item) => {
+//   return { ...item, subTotal: item.quantity * item.price };
+// });
+// console.log(withSubTotal);
