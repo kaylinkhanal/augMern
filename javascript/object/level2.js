7991d95ce5b55b7768bf63e1687af718c53e80ec
SHREESTIP <shristipradhan2056@gmail.com>
2024-09-08 08:49:28 +0545

    object level 2 answer

--- javascript/object/level2.js
+++ javascript/object/level2.js
@@ -6,6 +6,13 @@ const scores = {
   //calculate the percentage using for in
   //full marks of each subject is 100
   
-  for(let item in details){
-    console.log(item)
+  let sum = 0
+  let totalSub = 0
+  for(let item in scores){
+    //console.log(scores[item])
+    sum = sum + scores[item]
+    totalSub++
   }
+//console.log(sum);
+const percentage = sum/(totalSub*100)*100
+percentage.toFixed(2)
