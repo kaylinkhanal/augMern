daff215bd12116e0fec5fc2c7c0e66e42d17fc7a
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-08 08:34:57 +0545

    object level2 done

--- javascript/object/level2.js
+++ javascript/object/level2.js
@@ -1,11 +1,14 @@
 const scores = {
-    math: 53,
-    science:50,
-    computer: 54
-  }
-  //calculate the percentage using for in
-  //full marks of each subject is 100
-  
-  for(let item in details){
-    console.log(item)
-  }
+  math: 53,
+  science: 50,
+  computer: 54,
+};
+//calculate the percentage using for in
+//full marks of each subject is 100
+let sum = 0;
+for (let item in scores) {
+  sum = sum + scores[item];
+}
+console.log(sum);
+const percentage = (sum / 300) * 100;
+console.log(percentage);
