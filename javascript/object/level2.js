f11cf597493ef557105290cc4394e059dd39d4d0
Dilasha Shakya <dilashakya@gmail.com>
2024-09-07 21:47:21 -0500

    level 4 added

--- javascript/object/level2.js
+++ javascript/object/level2.js
@@ -1,11 +1,15 @@
 const scores = {
-    math: 53,
-    science:50,
-    computer: 54
-  }
-  //calculate the percentage using for in
-  //full marks of each subject is 100
-  
-  for(let item in details){
-    console.log(item)
-  }
+  math: 53,
+  science:50,
+  computer: 54
+}
+//calculate the percentage using for in
+//full marks of each subject is 100
+let sum = 0
+let number = 0 
+for(let item in scores){
+  sum = sum + scores[item]
+  number +=1
+}
+const percentage = sum/number*100*100
+console.log(percentage)
