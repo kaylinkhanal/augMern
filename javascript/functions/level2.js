c2424685ba86a208342188690d1839cbe2ad304f
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-09 13:06:34 +0545

    function level2 issue in answer solved

--- javascript/functions/level2.js
+++ javascript/functions/level2.js
@@ -1,14 +1,15 @@
-const cholocateCounter = (quantity, price) => {};
+const cholocateCounter = (quantity, price) => {
+  if (quantity * price < 100) {
+    return "Sorry, order less than of Rs. 100 is not allowed.";
+  } else {
+    return "Hi, please pay Rs." + quantity * price;
+  }
+};
 
 const price = cholocateCounter(3, 50);
 console.log(price);
 //Q1
 //expected output is 'Hi, please pay Rs.150'
-if (quantity * price < 100) {
-  return "sorry order less than of rs 100 is not allowed";
-} else {
-  "hi please pay Rs." + quantity * price;
-}
 //Q2
 //if 1,50 is passed instead of 3,50 expected output is 'Sorry, order less than of rs
 // 100 is not allowed '
