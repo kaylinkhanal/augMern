9c20d2447714d07327344dccdf889807d97ada60
Dilasha Shakya <dilashakya@gmail.com>
2024-09-09 17:49:00 -0500

    functions level 2 added

--- javascript/functions/level2.js
+++ javascript/functions/level2.js
@@ -1,13 +1,19 @@
 
 const cholocateCounter = (quantity, price)=> {
-
-}
-
-const price = cholocateCounter(3, 50)
-console.log(price)
-//Q1
-//expected output is 'Hi, please pay Rs.150'
-
-//Q2
-//if 1,50 is passed instead of 3,50 expected output is 'Sorry, order less than of rs
-// 100 is not allowed '
+    const output = price * quantity
+    if (output > 100){
+      return `Please pay Rs. ${output}`
+    }
+    else{
+      return 'Sorry, order less than of Rs 100 is not allowed '
+    }
+  
+  }
+  
+  const price = cholocateCounter(1, 50)
+  console.log(price)
+  //Q1
+  //expected output is 'Hi, please pay Rs.150'
+  
+  //Q2
+  //if 1,50 is passed instead of 3,50 expected output is 'Sorry, order less 
