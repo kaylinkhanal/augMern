842c323e8f9e9c47c1a264858a653d9e4e204ca1
SthaPradhim <74393584+SthaPradhim@users.noreply.github.com>
2024-09-09 09:04:54 +0545

    function level1 pushed with error

--- javascript/functions/level1.js
+++ javascript/functions/level1.js
@@ -1,9 +1,13 @@
-const name ="hello"
-function test(){
-    return name
+function test() {
+  return name;
 }
+console.log(name);
 
 //Q1 convert to arrow function
-
-
+test(() => {
+  return name;
+});
+console.log(name);
 //Q2 pass name as argument instead of external constants
+const test(name)=>{return name}
+test(name)
