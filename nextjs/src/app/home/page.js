367f541759247ef9a3bf60d8e6c1dc322d93c64d
Aaditya Paudel <78781844+adityaspaudel@users.noreply.github.com>
2024-10-02 14:04:17 +0545

    user & userList changed

---
+++ nextjs/src/app/home/page.js
@@ -0,0 +1,13 @@
+import Newpage from '@/components/newpage/page'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <span><Newpage/></span>
+      <div>Home</div>
+    </div>
+  )
+}
+
+export default Home
--- nextjs/src/components/userList/page.js
+++ nextjs/src/components/userList/page.js
@@ -1,26 +1,41 @@
-'use client'
-import { GrapeIcon, LeafyGreenIcon } from 'lucide-react'
-import React from 'react'
+"use client";
+import { CircleDot, Dot, GrapeIcon, LeafyGreenIcon } from "lucide-react";
+import React from "react";
 
 const UserListBox = (props) => {
+	return (
+		<div className="flex flex-col bg-white  w-[20%] h-screen shadow-md rounded-2xl p-2">
+			{props.userData.map((item) => {
+				return (
+					<div
+						onClick={() => props.setUserSelection(item)}
+						className="flex gap-3 p-4 mb-2 shadow-sm  rounded-xl">
+						<div className="relative">
+							<img
+								src={item.avatar}
+								className="rounded-[50%] h-[60px]"
+							/>
+							<div className="absolute top-0 left-1 ">
+								{" "}
+								{item.onlineStatus ? (
+									<Dot
+										className="text-green-500 drop-shadow-md"
+										size={100}
+									/>
+								) : (
+									<Dot
+										className="text-red-500 "
+										size={100}
+									/>
+								)}
+							</div>
+						</div>
+						<div className="font-semibold text-sm">{item.name}</div>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
-  return (
-    <div  className='flex flex-col bg-white  w-[20%] h-screen shadow-md rounded-2xl p-2'> 
-        {props.userData.map((item)=>{
-            return (<div onClick={()=>props.setUserSelection(item)} className='flex gap-3 p-4 mb-2 shadow-sm  rounded-xl'>
-         
-                <div className='relative'>
-                    <img src= {item.avatar} className='rounded-[50%] h-[50px]'/>
-                    <div className='absolute bottom-1 '> {item.onlineStatus ? <LeafyGreenIcon size={10}/> : <GrapeIcon size={10}/>}</div>
-                </div>
-                <div className='font-semibold text-sm'>
-                {item.name}
-                </div>
-           
-                </div>)
-        })}
-    </div>
-  )
-}
-
-export default UserListBox
+export default UserListBox;
