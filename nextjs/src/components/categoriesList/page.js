16831776de3855468ab981908aea5b68cdacc18e
Kaylin Khanal <kyalinnop@gmail.com>
2024-10-06 09:49:49 +0545

    add pagination demo

--- nextjs/src/components/categoriesList/page.js
+++ nextjs/src/components/categoriesList/page.js
@@ -1,29 +1,36 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import CategoriesCard from '../categoriesCard/page'
+"use client";
+import React, { useEffect, useState } from "react";
+import CategoriesCard from "../categoriesCard/page";
+import { PaginationDemo } from "../pagination-demo";
 
 const CategoriesList = () => {
-    const [categoriesList, setCategoriesList] = useState([])
-     useEffect(()=>{
-        fetchCategories()
-     },[])
+  const [categoriesList, setCategoriesList] = useState([]);
+  const [totalItems, setTotalItems] = useState(0)
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-     const fetchCategories = async ()=>{
-        const data = await fetch('https://api.escuelajs.co/api/v1/categories')
-        let categories = await data.json()
-        setCategoriesList(categories)
-
-     }
+  const fetchCategories = async () => {
+    const data = await fetch("https://api.escuelajs.co/api/v1/categories");
+    let categories = await data.json();
+    setTotalItems(categories.length)
+    setCategoriesList(categories);
+  };
   return (
-    <div className='flex gap-4'>
-        {
-        categoriesList.map((item)=>{
-            return (
-               <CategoriesCard item={item}/>
-            )
-        })
-        }</div>
-  )
-}
+    <div className="flex gap-4">
+      <div className="flex flex-col">
+     
+        <div className="flex">
+          {categoriesList.map((item) => {
+            return <CategoriesCard item={item} />;
+          })}
+        </div>
+        <div>
+          <PaginationDemo totalItems={totalItems}/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CategoriesList
+export default CategoriesList;
