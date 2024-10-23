9808390811265f6926096415fb489663897c855b
Aaditya Paudel <78781844+adityaspaudel@users.noreply.github.com>
2024-10-23 11:03:21 +0545

    nodejs module added for practice

---
+++ nodejs/001-home/page.js
@@ -0,0 +1,16 @@
+// var http = require("http");
+
+// http
+// 	.createServer(function (req, res) {
+// 		res.writeHead(200, { "Content-Type": "text/plain" });
+// 		res.end("Hello World!");
+// 	})
+// 	.listen(8080);
+
+var http = require("http");
+http
+	.createServer(function (req, res) {
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.end("hello world");
+	})
+	.listen(8080);
